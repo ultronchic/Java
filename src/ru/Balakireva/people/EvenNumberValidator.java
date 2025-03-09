6f738332cdc50a51ed50b2589e1732c3f2fa4cd1
ultronchic <ultronchic@mail.ru>
2025-03-09 19:54:51 +0300

    Delete src directory

--- src/ru/Balakireva/people/EvenNumberValidator.java
+++
@@ -1,8 +0,0 @@
-package ru.Balakireva.people;
-
-public class EvenNumberValidator implements GradeValidator {
-    @Override
-    public boolean isValid(int grade) {
-        return grade % 2 == 0;
-    }
-}
--- src/ru/Balakireva/people/ZeroOneValidator.java
+++
@@ -1,8 +0,0 @@
-package ru.Balakireva.people;
-
-public class ZeroOneValidator implements GradeValidator {
-    @Override
-    public boolean isValid(int grade) {
-        return grade == 0 || grade == 1;
-    }
-}
